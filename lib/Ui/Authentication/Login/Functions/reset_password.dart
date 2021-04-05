85ea3ab966af784eef85ee82add827a5a263f95c
MohamedAymanMohamedIbrahimEl-Adawy <mohamedadawy89.ma@gmail.com>
2021-04-06 01:48:09 +0200

    ok

--- lib/Ui/Authentication/Login/Functions/reset_password.dart
+++ lib/Ui/Authentication/Login/Functions/reset_password.dart
@@ -3,7 +3,8 @@ import 'package:url_launcher/url_launcher.dart';
 //
 
 void contactToResetPassword(String email) async {
-  final String url = "whatsapp://send?phone=966501722732&text=$email";
+  final String url =
+      "whatsapp://send?phone=966501722732&text=نسيت كلمة المرور$email";
   if (await canLaunch(url)) {
     await launch(url, forceSafariVC: false);
   } else {
